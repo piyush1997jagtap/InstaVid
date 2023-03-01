7d174b92f33973fb3199082707f63527dd4c15f8
Piyush <piyush1997jagtap@gmail.com>
2023-03-01 23:57:02 +0530

    Removed whiteboard code and added participant count

--- public/js/room.js
+++ public/js/room.js
@@ -13,128 +13,6 @@ const videoButt = document.querySelector('.novideo');
 const audioButt = document.querySelector('.audio');
 const cutCall = document.querySelector('.cutcall');
 const screenShareButt = document.querySelector('.screenshare');
-const whiteboardButt = document.querySelector('.board-icon')
-
-//whiteboard js start
-const whiteboardCont = document.querySelector('.whiteboard-cont');
-const canvas = document.querySelector("#whiteboard");
-const ctx = canvas.getContext('2d');
-
-let boardVisisble = false;
-
-whiteboardCont.style.visibility = 'hidden';
-
-let isDrawing = 0;
-let x = 0;
-let y = 0;
-let color = "black";
-let drawsize = 3;
-let colorRemote = "black";
-let drawsizeRemote = 3;
-
-function fitToContainer(canvas) {
-    canvas.style.width = '100%';
-    canvas.style.height = '100%';
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-}
-
-fitToContainer(canvas);
-
-//getCanvas call is under join room call
-socket.on('getCanvas', url => {
-    let img = new Image();
-    img.onload = start;
-    img.src = url;
-
-    function start() {
-        ctx.drawImage(img, 0, 0);
-    }
-
-    console.log('got canvas', url)
-})
-
-function setColor(newcolor) {
-    color = newcolor;
-    drawsize = 3;
-}
-
-function setEraser() {
-    color = "white";
-    drawsize = 10;
-}
-
-function reportWindowSize() {
-    fitToContainer(canvas);
-}
-
-window.onresize = reportWindowSize;
-
-function clearBoard() {
-    if (window.confirm('Are you sure you want to clear board? This cannot be undone')) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        socket.emit('store canvas', canvas.toDataURL());
-        socket.emit('clearBoard');
-    }
-    else return;
-}
-
-socket.on('clearBoard', () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-})
-
-function draw(newx, newy, oldx, oldy) {
-    ctx.strokeStyle = color;
-    ctx.lineWidth = drawsize;
-    ctx.beginPath();
-    ctx.moveTo(oldx, oldy);
-    ctx.lineTo(newx, newy);
-    ctx.stroke();
-    ctx.closePath();
-
-    socket.emit('store canvas', canvas.toDataURL());
-
-}
-
-function drawRemote(newx, newy, oldx, oldy) {
-    ctx.strokeStyle = colorRemote;
-    ctx.lineWidth = drawsizeRemote;
-    ctx.beginPath();
-    ctx.moveTo(oldx, oldy);
-    ctx.lineTo(newx, newy);
-    ctx.stroke();
-    ctx.closePath();
-
-}
-
-canvas.addEventListener('mousedown', e => {
-    x = e.offsetX;
-    y = e.offsetY;
-    isDrawing = 1;
-})
-
-canvas.addEventListener('mousemove', e => {
-    if (isDrawing) {
-        draw(e.offsetX, e.offsetY, x, y);
-        socket.emit('draw', e.offsetX, e.offsetY, x, y, color, drawsize);
-        x = e.offsetX;
-        y = e.offsetY;
-    }
-})
-
-window.addEventListener('mouseup', e => {
-    if (isDrawing) {
-        isDrawing = 0;
-    }
-})
-
-socket.on('draw', (newX, newY, prevX, prevY, color, size) => {
-    colorRemote = color;
-    drawsizeRemote = size;
-    drawRemote(newX, newY, prevX, prevY);
-})
-
-//whiteboard js end
 
 let videoAllowed = 1;
 let audioAllowed = 1;
@@ -159,8 +37,7 @@ let cName = {};
 let audioTrackSent = {};
 let videoTrackSent = {};
 
-let mystream, myscreenshare;
-
+let mystream;
 
 document.querySelector('.roomcode').innerHTML = `${roomid}`
 
@@ -214,9 +91,13 @@ socket.on('user count', count => {
     console.log(count, 'count');
     if (count > 1) {
         videoContainer.className = 'video-cont';
+        const badge = document.getElementById("userCount");
+        badge.innerHTML = `(${count})`;
     }
     else {
         videoContainer.className = 'video-cont-single';
+        document.getElementById("userCount").innerHTML = `(${count})`;
+
     }
 })
 
@@ -326,8 +207,6 @@ function handleVideoOffer(offer, sid, cname, micinf, vidinf) {
             videoContainer.appendChild(vidCont);
 
         }
-
-
     };
 
     connections[sid].onremovetrack = function (event) {
@@ -704,17 +583,6 @@ socket.on('action', (msg, sid) => {
         console.log(sid + 'turned video on');
         document.querySelector(`#vidoff${sid}`).style.visibility = 'hidden';
         videoInfo[sid] = 'on';
-    }
-})
-
-whiteboardButt.addEventListener('click', () => {
-    if (boardVisisble) {
-        whiteboardCont.style.visibility = 'hidden';
-        boardVisisble = false;
-    }
-    else {
-        whiteboardCont.style.visibility = 'visible';
-        boardVisisble = true;
     }
 })
 
